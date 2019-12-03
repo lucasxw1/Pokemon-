ae50894dfa128dabe87d35d12c18a3742e3c394f
Lucas Garcia <36937991+lucasxw1@users.noreply.github.com>
2019-12-03 05:24:57 -0300

    Add files via upload

--- AlestTrab/App.java
+++ AlestTrab/App.java
@@ -1,15 +1,43 @@
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+
+import javax.sound.sampled.SourceDataLine;
+
 public class App{
     public static void main(String[] args){
-        RedBlackBST<Integer, String> bst = new RedBlackBST<Integer, String>();
-        bst.put(1, "a");
-        bst.put(2,"b");
-        bst.put(3,"c");
-          bst.put(4, "d");
-        bst.put(9,"e");
-        bst.put(3,"f");
+        RedBlackBST<String, String> bst = new RedBlackBST<String, String>();
+      
+       try{
+        Scanner scanner =  new Scanner(new File("Pasta2.txt"));
+        scanner.useDelimiter("[-\n]");
+        scanner.next();
+    
+               
+   while (scanner.hasNext()) {
+      String hash = scanner.next();
+      String pokemon = scanner.next();
+      bst.add(hash, pokemon);
+   }
+
+  System.out.println(bst.positionsCentral());
+  System.out.println(bst.get("1"));
+  
+  
+ 
+
+
+
+  
+   
+  
+  
+  scanner.close();
+       }catch(Exception e){
+         System.out.println(e.getClass());
+       }
         
-        System.out.println(bst.getParent(1));
-         
 
     }
 
